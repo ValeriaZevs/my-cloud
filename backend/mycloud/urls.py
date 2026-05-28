ae552b4bd0f90a9a719cd404bed3dacc004d5220
ValeriaZevs <valeriazevs1912@mail.ru>
2026-05-28 12:47:58 +0300

    Добавлен обработчик для bundle.js и статики React

--- backend/mycloud/urls.py
+++ backend/mycloud/urls.py
@@ -1,8 +1,10 @@
+import os
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from storage.views import AdminToggleAdminView
 
 urlpatterns = [
@@ -12,6 +14,10 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    re_path(r'^(?P<path>.*\.(?:js|css|ico|png|jpg|jpeg|svg|gif|map))$', serve, {'document_root': os.path.join(settings.BASE_DIR, 'frontend_build')}),
+]
 
 urlpatterns += [
     re_path(r'^(?!api|admin|media).*$', TemplateView.as_view(template_name='index.html')),
