637169bc4624b3b60e6e6928e14d39c9780e244f
ValeriaZevs <valeriazevs1912@mail.ru>
2026-05-28 12:06:13 +0300

    Настройка логирования с датой и временем по ТЗ

--- backend/mycloud/settings.py
+++ backend/mycloud/settings.py
@@ -115,3 +115,24 @@ CSRF_TRUSTED_ORIGINS = [
     "http://194.67.92.55:3000", 
     "http://194.67.92.55:8000",
 ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',  
+    },
+}
