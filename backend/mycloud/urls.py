ab75c9ee33de5c1346cf2ac41c8f101b813c405d
ValeriaZevs <valeriazevs1912@mail.ru>
2026-05-28 12:44:27 +0300

    Добавлены недостающие импорты в urls.py

--- backend/mycloud/urls.py
+++ backend/mycloud/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
-from storage.views import AdminToggleAdminView
-from django.urls import path, re_path
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from storage.views import AdminToggleAdminView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,5 +14,5 @@ urlpatterns = [
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [
-    re_path(r'^(?!api/).*$', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^(?!api|admin|media).*$', TemplateView.as_view(template_name='index.html')),
 ]
