e84d14a25ee392dc7cd289406eaebc4ccf86becf
ValeriaZevs <valeriazevs1912@mail.ru>
2026-05-28 12:31:31 +0300

    Переход на единый сервер: сборка React и раздача через Django

--- backend/mycloud/settings.py
+++ backend/mycloud/settings.py
@@ -50,10 +50,11 @@ ROOT_URLCONF = 'mycloud.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend_build')], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -99,7 +100,13 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend_build', 'static'),
+    os.path.join(BASE_DIR, 'frontend_build'),
+]
 AUTH_USER_MODEL = 'storage.User'
 
 
--- backend/mycloud/urls.py
+++ backend/mycloud/urls.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 from storage.views import AdminToggleAdminView
+from django.urls import path, re_path
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('storage.urls')), 
     path('api/admin/users/<int:pk>/toggle_admin/', AdminToggleAdminView.as_view()),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    re_path(r'^(?!api/).*$', TemplateView.as_view(template_name='index.html')),
 ]
