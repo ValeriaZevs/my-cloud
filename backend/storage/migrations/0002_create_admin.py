b042aebafd17de5923860bcd0115a12fa8abc429
ValeriaZevs <valeriazevs1912@mail.ru>
2026-05-28 12:18:54 +0300

    Исправление модели пользователя в миграции создания админа

--- backend/storage/migrations/0002_create_admin.py
+++ backend/storage/migrations/0002_create_admin.py
@@ -2,7 +2,7 @@ from django.db import migrations
 from django.contrib.auth.hashers import make_password
 
 def create_superuser(apps, schema_editor):
-    User = apps.get_model('auth', 'User')
+    User = apps.get_model('storage', 'User')
     
     if not User.objects.filter(username='admin').exists():
         User.objects.create(
@@ -16,7 +16,6 @@ def create_superuser(apps, schema_editor):
 class Migration(migrations.Migration):
     dependencies = [
         ('storage', '0001_initial'),
-        ('auth', '0012_alter_user_first_name_max_length'),
     ]
 
     operations = [
